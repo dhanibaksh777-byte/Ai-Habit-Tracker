18f7326d3ef387ea192fd87467ec0c6ad5ead269
dhanibaksh777-byte <dhanibaksh781@gmail.com>
2026-08-23 01:54:41 +0500

    Update models and schemas

--- app/models.py
+++ app/models.py
@@ -1,9 +1,18 @@
 from app.database import base
-from sqlalchemy import Column,String,Boolean,Integer,DateTime,ForeignKey
+from sqlalchemy import Column,String,Boolean,Integer,DateTime,ForeignKey,Date
 from datetime import datetime,timezone
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy import Date
+from datetime import date as date_type
+from sqlalchemy import UniqueConstraint
+import enum
 import uuid
+
+class HabitStatus(str,enum.Enum):
+    DONE = "done"
+    SKIPPED = "skipped"
 
 class User(base):
     __tablename__ = "users"
@@ -23,9 +32,13 @@ class Habit(base):
     user_id = Column(ForeignKey("users.id"))
     owner = relationship("User",back_populates="habits")
 
-
-
-
-
-
+class HabitLog(base):
+    __tablename__ = "habitlogs"
+    id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    habit_id = Column(ForeignKey("habits.id"))
+    status = Column(SQLEnum(HabitStatus),nullable=False)
+    date = Column(Date,default=date_type.today)
+    __table_args__ = (
+        UniqueConstraint("habit_id", "date", name="uq_habit_date"),
+    )
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel,EmailStr
 from uuid import UUID
+from app.models import HabitStatus
+from datetime import date as date_type
 #USER SCHEMAS:
 class CreateUser(BaseModel):
     username : str 
@@ -32,3 +34,13 @@ class HabitResponse(BaseModel):
 class UpdatedHabit(BaseModel):
     updated_name : str
 
+
+#HabitLogs:
+class CreateLog(BaseModel):
+    status: HabitStatus
+
+class LogResponse(BaseModel):
+    id : UUID
+    habit_id : UUID
+    date : date_type
+    status : HabitStatus
